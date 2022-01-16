8bb287aaf174a9f0a325870f8783d173e3c92edd
ai03-2725 <admin@ai03.com>
2022-01-17 02:51:06 +0900

    Begin work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <h1>Test</h1>
+      <p>Plategen will happen here</p>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "bootswatch/dist/lux/bootstrap.min.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
